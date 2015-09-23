4081ac7652d7742f3428f2c7a6b77539fd25c53b
Valmorbus <borgstrom.simon@gmail.com>
2015-09-23 09:11:21 +0200

    catch | fix

--- OOPJ15/src/vecka1LektionerIO/CharStreams.java
+++ OOPJ15/src/vecka1LektionerIO/CharStreams.java
@@ -29,7 +29,7 @@ public class CharStreams {
 				}
 			
 		}
-		catch (FileNotFoundException e) {
+		catch (FileNotFoundException | ArithmeticException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}catch (IOException e1) {
