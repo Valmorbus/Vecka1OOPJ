59ead30d9d5c28bb33582b27cba996f34aa70f49
Valmorbus <borgstrom.simon@gmail.com>
2015-09-30 12:02:18 +0200

    added generic linked list

---
+++ OOPJ15/src/vecka2/linkedlist/TestLinkedList.java
@@ -0,0 +1,45 @@
+package vecka2.linkedlist;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+
+public class TestLinkedList {
+
+	public static void main(String[] args) throws IOException {
+
+		LinkedList<String> test = new LinkedList<String>();
+
+		test = readFile();
+
+		System.out.println(test.getSize());
+		System.out.println(test.tostring());
+		test.add("hej");
+		System.out.println(test.getSize());
+
+	}
+
+	public static LinkedList<String> readFile()
+	{
+			File file = new File("C:/Users/borgs_000/git/Vecka1OOPJ/OOPJ15/src/vecka2/linkedlist/test.txt");
+			String str ="";
+			LinkedList<String> test = new LinkedList<String>();
+			
+		    try (BufferedReader br = new BufferedReader(new FileReader(file))){
+		        while((str =br.readLine()) !=null) {        
+		            //str =br.readLine();
+		            test.add(str);
+		        }
+		       
+		    } catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+		    return test;
+	}
+
+}
--- OOPJ15/src/vecka2/shapes/Calculator.java
+++ OOPJ15/src/vecka2/shapes/Calculator.java
@@ -1,18 +1,17 @@
 package vecka2.shapes;
 
 public class Calculator{
-	
-	//Check where math error is 
-	
+
+	//fulhack med uträkningarna
 	public static void main(String[] args)
 	{
 		int x = 1;
 		Shape circleShape = new Circle(x*1.0f);
 		Shape smallCircleShape = new Circle(x*0.5f);
 		Shape rectangleShape = new Rectangle(x*3.0f, x*1.0f);
-		System.out.println((circleShape.calculateArea()/2)+rectangleShape.calculateArea()+(smallCircleShape.calculateArea()*2));
+		System.out.println((circleShape.calculateArea()/2)+rectangleShape.calculateArea()+(smallCircleShape.calculateArea()));
 		
-		System.out.println((circleShape.calculateCircumference()/2)+rectangleShape.calculateCircumference()+(smallCircleShape.calculateCircumference()*2));
+		System.out.println((circleShape.calculateCircumference()/2)+rectangleShape.calculateCircumference()/2+(smallCircleShape.calculateCircumference()));
 		
 		
 	}
